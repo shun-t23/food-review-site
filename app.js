2048dc545df9492e5b9616ea0423dfebb17d023a
shunsuke <shuntanaka01@outlook.jp>
2024-08-19 15:37:30 +0900

    [add]セッションハイジャック対策

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 const appconfig = require('./config/application.config.js');
 const dbconfig = require('./config/mysql.config.js');
 const path = require('path');
@@ -59,6 +60,9 @@ app.use(
       password: dbconfig.PASSWORD,
       database: dbconfig.DATABASE,
     }),
+    cookie: {
+      secure: IS_PRODUCTION,
+    },
     secret: appconfig.security.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
